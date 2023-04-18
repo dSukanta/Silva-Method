7dfe5360d68605271860dfdb5f0b526ba527fc61
dsukanta <sukanta@semicolonites.com>
2023-04-18 17:42:14 +0530

    responsive home

--- src/pages/Home/Home/HomeOurTeam/HomeOurTeam.js
+++ src/pages/Home/Home/HomeOurTeam/HomeOurTeam.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeSingleTeam from '../../../../components/HomeSingleTeam/HomeSingleTeam';
+import { useMediaQuery } from 'react-responsive'
 
 const HomeOurTeam = () => {
+   const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1280px)'})
+   const isTablet = useMediaQuery({ minWidth: 481, maxWidth: 768 })
+   const isMobile = useMediaQuery({minWidth: 320, maxWidth:480  })
+
    return (
       <>
-         <section className="team-area pt-115 pb-55">
+         <section className="team-area pt-115 pb-5">
             <div className="container">
                <div className="row">
                   <div className="col-xl-6 col-lg-7 col-md-10">
@@ -28,10 +33,10 @@ const HomeOurTeam = () => {
                      </div> */}
                   </div>
                </div>
-               <div className="row">
+               <div style={{display:'grid', gridTemplateColumns: isMobile? '1fr 1fr':'1fr 1fr 1fr 1fr', gap:"1rem"}}>
 
                   <HomeSingleTeam src={"https://silvamethod.com/assets/images/buy-silva-mind-control-book.webp"} title="THE SILVA MIND CONTROL METHOD" subtitle="by Jose Silva and Philip Miele" />
-                  <HomeSingleTeam src={"https://silvamethod.com/assets/images/Silva%20method%20Life%20System%20Homestudy%20Program.webp"} title="Silva Life System - Home Study Programs" subtitle="by Silva Methodt" />
+                  <HomeSingleTeam src={"https://silvamethod.com/assets/images/Silva%20method%20Life%20System%20Homestudy%20Program.webp"} title="Silva Life System - Home Study Programs" subtitle="by Silva Method" />
                   <HomeSingleTeam src={"https://silvamethod.com/assets/images/i-have-a-lunch.webp"} title="I Have a Hunch: The Autobiography of Jose Silvan" subtitle="by Jose Silva" />
                   <HomeSingleTeam src={"https://silvamethod.com/assets/images/pathways-to-personal-freedom-using-the-silva-method.webp"} title="Pathways to Personal Freedom using the Silva Method" subtitle="by Diana Silva and Robert Deutchmant" />
                   <HomeSingleTeam src={"https://silvamethod.com/assets/images/silva-long-relax-exercise.webp"} title="Silva Long Relax Exercisee" subtitle="by Jose Silva (Digital)" />
