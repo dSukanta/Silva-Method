179ef1ec00e043c8642d87c0078be2d178825dc5
dsukanta <sukanta@semicolonites.com>
2023-05-04 16:42:10 +0530

    updated

--- src/pages/Store/SingleLecturePage/SingleLectureFooter.js
+++ src/pages/Store/SingleLecturePage/SingleLectureFooter.js
@@ -37,39 +37,39 @@ function SingleLectureFooter({
   //let currentChapter = 0;
 // let currentLesson = 0;
 
-function next(){
-    console.log("function running");
-    let currentlessonslength = chapters[currentChapter].lession.length-1;
-    if(currentLesson+1>currentlessonslength){
-        if(chapters[currentChapter+1].lession[0]){
-            setCurrentChapter(++currentChapter)
-            setCurrentLesson(0)
-        }
-    }else{
-        setCurrentLesson(++currentLesson)
-    }
-}
+// function next(){
+//     console.log("function running");
+//     let currentlessonslength = chapters[currentChapter].lession.length-1;
+//     if(currentLesson+1>currentlessonslength){
+//         if(chapters[currentChapter+1].lession[0]){
+//             setCurrentChapter(++currentChapter)
+//             setCurrentLesson(0)
+//         }
+//     }else{
+//         setCurrentLesson(++currentLesson)
+//     }
+// }
 
-function prev(){
-    if(currentLesson!==0){
-       setCurrentLesson( --currentLesson)
-    }else{
-       let currentlessonslength = chapters[currentChapter-1].lession.length-1;
-       if(currentlessonslength){
-           if(chapters[currentChapter-1].lession[currentlessonslength]){
-           setCurrentChapter(--currentChapter)
-           setCurrentLesson(currentlessonslength)
-       }
-       }
-    }
-}
+// function prev(){
+//     if(currentLesson!==0){
+//        setCurrentLesson( --currentLesson)
+//     }else{
+//        let currentlessonslength = chapters[currentChapter-1].lession.length-1;
+//        if(currentlessonslength){
+//            if(chapters[currentChapter-1].lession[currentlessonslength]){
+//            setCurrentChapter(--currentChapter)
+//            setCurrentLesson(currentlessonslength)
+//        }
+//        }
+//     }
+// }
 
 
   return (
     <Navbar>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-between">
-        <Navbar.Text className="mx-5 text-dark" onClick={prev}>
+        <Navbar.Text className="mx-5 text-dark">
           <GrLinkPrevious style={{ marginRight: "10px" }} size={25} /> Previous
         </Navbar.Text>
         {/* {!marked && (
@@ -88,7 +88,7 @@ function prev(){
             <TiTickOutline color="green" /> Completed
           </h4>
         )} */}
-        <Navbar.Text className="mx-5 text-dark" onClick={next}>
+        <Navbar.Text className="mx-5 text-dark">
           Next <GrLinkNext style={{ marginLeft: "10px" }} size={25}/>
         </Navbar.Text>
       </Navbar.Collapse>
