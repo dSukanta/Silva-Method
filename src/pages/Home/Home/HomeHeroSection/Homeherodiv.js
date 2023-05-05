4da4abb568bf1bfca9c338648fbb250e7be28eaf
dsukanta <sukanta@semicolonites.com>
2023-05-05 12:33:07 +0530

    updated

--- src/pages/Home/Home/HomeHeroSection/Homeherodiv.js
+++ src/pages/Home/Home/HomeHeroSection/Homeherodiv.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useMediaQuery } from 'react-responsive'
+import { Link } from 'react-router-dom'
 
 function Homeherodiv() {
   const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1280px)'})
@@ -9,7 +10,7 @@ function Homeherodiv() {
     <div style={{display: 'flex' , flexDirection:isDesktopOrLaptop? 'row':'column-reverse', background:'rgb(73,64,224)',backgroundImage: "linear-gradient(#3420e7, #da01fd)", textAlign:'center', justifyContent:'center', alignItems:'center', gap: isDesktopOrLaptop?'1rem':'0', marginBottom:'20px'}}>
         <div>
             {isDesktopOrLaptop? <h1 style={{color:'white',}}>Step into our Grateness</h1>: <h3 style={{color:'white'}}>Step into our Grateness</h3>}
-            <button className='home-join-btn'>Join Silva Method Now</button>
+            <Link to="/store/courses" className="primary_btn">Explore Courses</Link>
         </div>
         <div style={{width:isDesktopOrLaptop? '70%':'100%'}}>
         <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" x-webkit-airplay="allow" width='100%' preload='auto' playsInline='playsinline' loop muted autoPlay></video>
